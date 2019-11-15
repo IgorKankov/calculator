7d643c77255394ad1618d07e129863747385b036
Igor <Kankovigor@gmail.com>
2019-11-15 15:39:55 +0000

    final

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
 const display = document.querySelector('.calc .display');
 document.querySelectorAll('.calc .digits button, .calc .opers button, .calc .clr button, .calc .bck-spc button, .calc .sqrt button')
-    .forEach( button => button.addEventListener('click', digitOperPressed));
+    .forEach(button => button.addEventListener('click', digitOperPressed));
 
 function digitOperPressed(event) {
     const btnText = event.target.innerText;
     display.value += btnText;
-
-}
-
-document.querySelector('.calc .eq')
-    .addEventListener('click', eqPressed);
-
-function eqPressed() {
-    display.value = eval(display.value);
 }
 
 
+    document.querySelector('.calc .eq')
+        .addEventListener('click', eqPressed);
+
+    function eqPressed() {
+        display.value = eval(display.value);
+    }
 
 
-document.querySelector('.calc .clr')
-    .addEventListener('click', clrPressed)
 
-function clrPressed() {
-    display.value = ('')
-}
 
-document.querySelector('.calc .bck-spc')
-    .addEventListener('click', bckSpcPressed)
+    document.querySelector('.calc .clr')
+        .addEventListener('click', clrPressed)
 
-function bckSpcPressed() {
-    display.value = (display.value.substring(0, display.value.length - 1))
-}
-document.querySelector('.calc .sqrt')
-    .addEventListener('click', sqrtPressed)
+    function clrPressed() {
+        display.value = ('')
+    }
 
-function sqrtPressed() {
-    display.value = Math.sqrt(eval(display.value))
-}
+    document.querySelector('.calc .bck-spc')
+        .addEventListener('click', bckSpcPressed)
+
+    function bckSpcPressed() {
+        display.value = (display.value.substring(0, display.value.length - 1))
+    }
+    document.querySelector('.calc .sqrt')
+        .addEventListener('click', sqrtPressed)
+
+    function sqrtPressed() {
+        display.value = Math.sqrt(eval(display.value))
+    }
 
 
